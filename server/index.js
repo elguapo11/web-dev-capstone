88bb4c9cff095f1da365ae69304777c103cf7640
Benny Mendoza <bennybc9387@gmail.com>
2022-09-13 19:41:41 -0600

    built out some crud

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // server/index.js
 
 const express = require("express");
+const journalEntryController = require('./journalEntryController')
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +11,11 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.get('/api/journalentries', journalEntryController.getJournalEntries)
+
+//this line of code is pulling from journalentrycontroller, and we used "object.property", which makes reading the code easier, instead of req, res. 
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
---
+++ server/journalEntryController.js
@@ -0,0 +1,16 @@
+//functions itself go here
+
+module.exports = { 
+    getJournalEntries: (req, res) => {
+        res.status(200).send('ok')
+    }, 
+    putJournalEntries: (req, res) => {
+        res.status(200).send('dope')
+    }, 
+    postJournalEntries: (req, res) => {
+        res.sendStatus(200)
+    }
+}
+
+
+//this file is the logic that we are using in our index file. 
