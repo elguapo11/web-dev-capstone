9722b7bac6a2a315459d580871cbeffe9599fe0c
Benny Mendoza <bennybc9387@gmail.com>
2022-09-29 19:03:06 -0600

    fixed sql tables

--- db/setup/seed.sql
+++ db/setup/seed.sql
@@ -2,19 +2,23 @@ CREATE TABLE users(
     user_id SERIAL PRIMARY KEY, 
     email VARCHAR,
     first_name VARCHAR(50),
-    last_name VARCHAR(50),
+    last_name VARCHAR(50)
 );
 
 CREATE TABLE journal_entry (
     journal_entry_id SERIAL PRIMARY KEY, 
-    journal_entry TEXT REFERENCES email,
-    journal_timestamp TIMESTAMP,
+    journal_text VARCHAR(500),
+    journal_title VARCHAR(50), 
+    user_id_fk INT REFERENCES users(user_id),
+    date_created TIMESTAMP
     );
 
 
 CREATE TABLE comments (
     comments_id SERIAL PRIMARY KEY, 
-    user_comments TEXT REFERENCES email,
-    user_comments TIMESTAMP,
+    comment_text VARCHAR(500),
+    journal_entry_fk INT REFERENCES journal_entry(journal_entry_id),
+    user_id_fk REFERENCES users(user_id),
+    date_created TIMESTAMP
     
 );
