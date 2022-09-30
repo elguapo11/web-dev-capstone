195254939c0c867fc09aa758ec986728a394ea79
Benny Mendoza <bennybc9387@gmail.com>
2022-09-29 18:32:19 -0600

    added serial primarykey

--- db/setup/seed.sql
+++ db/setup/seed.sql
@@ -1,16 +1,19 @@
 CREATE TABLE users(
-    email VARCHAR PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY, 
+    email VARCHAR,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
 );
 
 CREATE TABLE journal_entry (
+    journal_entry_id SERIAL PRIMARY KEY, 
     journal_entry TEXT REFERENCES email,
     journal_timestamp TIMESTAMP,
     );
 
 
 CREATE TABLE comments (
+    comments_id SERIAL PRIMARY KEY, 
     user_comments TEXT REFERENCES email,
     user_comments TIMESTAMP,
     
