dd13a6a9c160a4f4fa97d2d3f84bb4c9ccdaac26
Benny Mendoza <bennybc9387@gmail.com>
2022-10-23 23:14:38 -0600

    reformatted

--- client/src/Entries.js
+++ client/src/Entries.js
@@ -1,27 +1,27 @@
 import { useRef } from "react";
 
-
 function Create() {
-  const titleRef = useRef()
-  const entryRef = useRef()
+  const titleRef = useRef();
+  const entryRef = useRef();
 
   function onSubmit(e) {
-    e.preventDefault()
-    console.log({text: titleRef.current.value, entry: entryRef.current.value})
+    e.preventDefault();
+    console.log({
+      text: titleRef.current.value,
+      entry: entryRef.current.value,
+    });
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="title">Title</label>
+      <input ref={titleRef} type="title" id="title" />
+      <span></span>
+      <label htmlFor="title">Start Writing</label>
+      <input ref={entryRef} type="entry" id="entry" />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
-return (
-  <form onSubmit={onSubmit}>
-    <label htmlFor="title">Title</label>
-    <input ref= {titleRef} type= "title" id="title"/>
-    <span></span>
-    <label htmlFor="title">Start Writing</label>
-    <input ref= {entryRef} type= "entry" id="entry"/>
-    <button type="submit">Submit</button>
-    </form>
-  )}
-
-
-
-
-  export default Create;
+export default Create;
