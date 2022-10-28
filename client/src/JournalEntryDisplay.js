b1c90921212e78c3d10c64d0a2c77901a7734a98
Benny Mendoza <bennybc9387@gmail.com>
2022-10-27 20:03:03 -0600

    deleting li

--- client/src/JournalEntryDisplay.js
+++ client/src/JournalEntryDisplay.js
@@ -1,55 +1,36 @@
 import { useEffect, useState } from "react";
-import axios from "axios"; 
-
+import axios from "axios";
 
 function JournalEntryDisplay() {
   const [retrieveEntries, setRetrieveEntries] = useState([]);
   useEffect(() => {
     axios.get("/api/journalentries").then((res) => {
-
-      setRetrieveEntries(res.data)
-
-
-    })
-      
+      setRetrieveEntries(res.data);
+    });
   }, []);
 
-    return( 
-      
-      
-      
-      <div>
-            <div
-                style={{
-                  display: 'flex',
-                  alignSelf: 'self',
-                  height: '100vh',
-                  color: "blue", 
-                }}
-            >
-                {retrieveEntries.map( (entry, index) => {
-                  return (
-                  <div key={index}>
-                    <div>
-                      <h2><li>{entry.journal_text}</li></h2>
-                    </div>
-                  </div>
-          )   
-            
-        }
-
-        )}
-                
-                
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          alignSelf: "self",
+          height: "100vh",
+          color: "blue",
+        }}
+      >
+        {retrieveEntries.map((entry, index) => {
+          return (
+            <div key={index}>
+              <div>
+                <h2>{entry.journal_text}</h2>
+              </div>
             </div>
-                
-                
-               
-
-        
+          );
+        })}
       </div>
-
-    )
+    </div>
+  );
 }
 
-export default JournalEntryDisplay;   
+export default JournalEntryDisplay;
