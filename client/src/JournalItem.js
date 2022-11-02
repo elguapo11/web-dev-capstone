4f935158f53b525f401162ce905b2dff9eacfc4f
Benny Mendoza <bennybc9387@gmail.com>
2022-11-01 20:36:30 -0600

    final CSS changes

--- client/src/JournalItem.js
+++ client/src/JournalItem.js
@@ -4,11 +4,11 @@ const JournalItem= (props) => {
 
         <div key={entry.journal_entry_id}>
         <div>
-          <h1 htmlFor="Journal Entry Number"> Entry number: {entry.journal_entry_id}</h1>
+          <h4 htmlFor="Journal Entry Number"> Entry number: {entry.journal_entry_id}</h4>
           <h2 htmlFor="title"> Title: {entry.journal_title}</h2>
             <ul>
-              <li htmlFor="text">Thoughts of the day: {entry.journal_text}</li>
-              <li htmlFor="Date Created">Date Created: {entry.date_created}</li>
+              <p htmlFor="text">Thoughts of the day: {entry.journal_text}</p>
+              <p htmlFor="Date Created">Date Created: {entry.date_created}</p>
             </ul>
         </div>
       </div>
